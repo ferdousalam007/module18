3d1326e71d500f4bd3229b2e6ca1ed7a77b68ba6
ferdousalam007 <iamferdousalam007@gmail.com>
2021-08-03 13:12:30 +0600

    function complete

---
+++ module18.js
@@ -0,0 +1,35 @@
+//function
+function sumNumbers(num1,num2){
+    var sum = num1 + num2;
+    return sum;
+}
+var inputNumbers1 = sumNumbers(10,20);
+console.log(inputNumbers1);
+
+//function 2
+function biogNumbers(num5,num6){
+    var biog= num5 - num6;
+    return biog;
+}
+var inputNumbers2 = biogNumbers(30,20);
+console.log("biog number" +" " +inputNumbers2);
+
+
+
+// //function 3
+function multiplyNumbers(num3,num4){
+     var multiply = num3 *num4;
+     return multiply;
+
+}
+var multiplyresult =multiplyNumbers(10,20);
+console.log(multiplyresult);
+
+
+// // function 3
+function vagfolNumber(num7,num8){
+     var vagfol = num7/num8;
+     return vagfol;
+}
+var vagfolResult = vagfolNumber(30,10);
+console.log(vagfolResult);
