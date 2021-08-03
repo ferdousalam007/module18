6018c6ed76b807a4dae74557a8ad9a01a6dcf480
ferdousalam007 <iamferdousalam007@gmail.com>
2021-08-03 16:54:57 +0600

    break & continue statement

---
+++ loopBreakContinue.js
@@ -0,0 +1,29 @@
+//while loop break staments
+// var i = 0;
+// while(i<15){
+//    i++
+// console.log(i);
+// if(i==4){
+//     break;
+// }
+// i++;
+// }
+// for loop for break staements
+// for(var a = 0; a<10; a++){
+//     console.log(a);
+//     if(a==6){
+//         break;
+//     }
+// }
+
+
+
+//while loop continue staments
+var i = 0;
+while(i<15){
+    i++
+if(i===4){
+  continue;
+}
+console.log(i);
+}
