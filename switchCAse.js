07acff738e3620917ce5cb6fba3c8bbeba8a05c0
ferdousalam007 <iamferdousalam007@gmail.com>
2021-08-03 16:37:40 +0600

    switch case statement

---
+++ switchCAse.js
@@ -0,0 +1,15 @@
+var color = "yellow";
+switch(color){
+case "red":
+    console.log("this is red");
+    break;
+ case "green":
+     console.log("this is green");
+     break;
+ case "yellow":
+     console.log("this is yellow");
+     break;
+  default:
+      console.log("color not matching");
+
+}
