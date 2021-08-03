4fb775fed621b5cf4eb13aedb283ea639c3acb3f
ferdousalam007 <iamferdousalam007@gmail.com>
2021-08-03 16:24:40 +0600

    object end

--- object.js
+++ object.js
@@ -1,1 +1,18 @@
-console.log("create object")
+var student1 = {
+    id: 001,
+    name: "parag",
+    marks:85
+}
+var student2 = {
+    id: 002,
+    name: "Ferdous",
+    marks:85
+}
+student1.marks =99;
+student1["name"] ="parag alam";
+student2["name"]= "ferdous Alam";
+var studentIdOne = student1.id;
+
+console.log(studentIdOne);
+console.log(student1.name);
+console.log(student2);
